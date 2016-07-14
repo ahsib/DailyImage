96d3080bf5f1abee6ce46f0e7546874f386008d5
ahsib <cscoi009@cscoi009ui-iMac.local>
2016-07-14 11:25:34 +0900

    fisrt_commit
    처음으로 올리는  커밋입니다.

---
+++ DailyImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyImage
+//
+//  Created by cscoi009 on 2016. 7. 14..
+//  Copyright © 2016년 Heo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
